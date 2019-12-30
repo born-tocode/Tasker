7f8371c8185436209da9aeee38056ca08c40fd89
Mateusz Boguszewski <mateo.b@data.pl>
2019-12-30 17:48:19 +0100

    jdbcAuthentication and User Entity

--- src/main/java/pl/borntocode/tasker/TaskController.java
+++ src/main/java/pl/borntocode/tasker/TaskController.java
@@ -2,23 +2,20 @@ package pl.borntocode.tasker;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
 
 @Slf4j
-@RestController
+@Controller
+@SessionAttributes("task")
 public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
-
-    @GetMapping("/")
-    public String indexPage() {
-        return "index";
-    }
 
     @GetMapping("/alltasks")
     public List<Task> retrieveAllTasks() {
--- src/main/java/pl/borntocode/tasker/security/SecurityConfig.java
+++ src/main/java/pl/borntocode/tasker/security/SecurityConfig.java
@@ -1,10 +1,28 @@
 package pl.borntocode.tasker.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username, password, enabled from Users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username, authority from userAuthorities where username=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
