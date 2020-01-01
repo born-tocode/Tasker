e2b80360b182b33b34d0e58739f181345f565381
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-01 13:51:54 +0100

    registration controller and registerForm View template

---
+++ src/main/java/pl/borntocode/tasker/RegistrationController.java
@@ -0,0 +1,25 @@
+package pl.borntocode.tasker;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.borntocode.tasker.data.UserRepository;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    private UserRepository userRepo;
+    private PasswordEncoder encoder;
+
+    public RegistrationController(UserRepository userRepo, PasswordEncoder encoder) {
+        this.userRepo = userRepo;
+        this.encoder = encoder;
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "register";
+    }
+}
