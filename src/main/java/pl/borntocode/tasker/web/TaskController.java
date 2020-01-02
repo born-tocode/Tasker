a2d04b1c8aeb3a1492e333692079d25ed7eb5671
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-02 23:13:50 +0100

    moved to tasker.web

--- src/main/java/pl/borntocode/tasker/web/TaskController.java
+++ src/main/java/pl/borntocode/tasker/web/TaskController.java
@@ -1,4 +1,4 @@
-package pl.borntocode.tasker;
+package pl.borntocode.tasker.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import pl.borntocode.tasker.Task;
 import pl.borntocode.tasker.data.TaskRepository;
 
 @Slf4j
