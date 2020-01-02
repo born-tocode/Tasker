05c5fd7641310a34caca975a4937a5984f082d09
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-02 23:10:58 +0100

    signIn Controller

--- src/main/java/pl/borntocode/tasker/security/SignInController.java
+++ src/main/java/pl/borntocode/tasker/security/SignInController.java
@@ -1,32 +1,26 @@
-package pl.borntocode.tasker;
+package pl.borntocode.tasker.security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.borntocode.tasker.data.UserRepository;
 
 @Controller
-@RequestMapping("/signup")
-public class SignUp {
+@RequestMapping("/signin")
+public class SignInController {
 
     private UserRepository userRepo;
     private PasswordEncoder encoder;
 
-    public SignUp(UserRepository userRepo, PasswordEncoder encoder) {
+    public SignInController(UserRepository userRepo, PasswordEncoder encoder) {
         this.userRepo = userRepo;
         this.encoder = encoder;
     }
 
     @GetMapping
-    public String registerForm() {
-        return "signup";
+    public String signInForm() {
+        return "signin";
     }
 
-    @PostMapping
-    public String processRegistrationForm(SignUpForm form) {
-        userRepo.save(form.toUser(encoder));
-        return "redirect:/signin";
-    }
 }
