6087609ad793e85bb35fb90e0a204813f859a1ce
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-02 11:17:46 +0100

    authorization users for tasks entries

--- src/main/java/pl/borntocode/tasker/security/SecurityConfig.java
+++ src/main/java/pl/borntocode/tasker/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,9 +21,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
-    @Qualifier("inMemoryUserDetailsManager")
+    @Qualifier("userRepositoryUserDetailsService")
     @Autowired
-    UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder encoder() {
@@ -41,4 +42,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(encoder());
     }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/alltasks", "/task/**")
+                .hasRole("USER")
+                .antMatchers("/", "/**").permitAll();
+    }
+
 }
