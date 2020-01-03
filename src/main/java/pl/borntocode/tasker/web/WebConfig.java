b974810a9ccf6437c9aeeeb200cd355b63ff362e
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-03 22:16:26 +0100

    custom error path in WebConfig

--- src/main/java/pl/borntocode/tasker/web/WebConfig.java
+++ src/main/java/pl/borntocode/tasker/web/WebConfig.java
@@ -1,15 +1,21 @@
 package pl.borntocode.tasker.web;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, ErrorController {
 
         @Override
         public void addViewControllers(ViewControllerRegistry registry) {
             registry.addViewController("/").setViewName("index");
             registry.addViewController("/signin");
         }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
 }
