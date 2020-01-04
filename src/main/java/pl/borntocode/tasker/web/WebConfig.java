3d34f8d63c03acc19a0a07ad560fc4bba647d661
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-04 11:29:23 +0100

    reformat code

--- src/main/java/pl/borntocode/tasker/web/WebConfig.java
+++ src/main/java/pl/borntocode/tasker/web/WebConfig.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer, ErrorController {
 
-        @Override
-        public void addViewControllers(ViewControllerRegistry registry) {
-            registry.addViewController("/").setViewName("index");
-            registry.addViewController("/signin");
-        }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/signin");
+    }
 
     @Override
     public String getErrorPath() {
