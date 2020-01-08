757168d324346897ba9d6347d49a053201ca3e83
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-08 19:31:53 +0100

    implemented Home controller

---
+++ src/main/java/pl/borntocode/tasker/web/HomeController.java
@@ -0,0 +1,15 @@
+package pl.borntocode.tasker.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String displayIndexPage() {
+        return "index";
+    }
+}
