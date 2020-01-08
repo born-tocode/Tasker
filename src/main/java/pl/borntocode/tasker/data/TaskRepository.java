28d6eb32d215917f9a0b7cd5a1b5a7234ecbf5ba
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-08 19:42:01 +0100

    done as JPA Repository

--- src/main/java/pl/borntocode/tasker/data/TaskRepository.java
+++ src/main/java/pl/borntocode/tasker/data/TaskRepository.java
@@ -1,11 +1,11 @@
 package pl.borntocode.tasker.data;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.borntocode.tasker.Task;
 
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
     Task readByTaskContains(String string);
 }
