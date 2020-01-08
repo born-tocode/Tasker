9795b9fe5ecdb921bd78fb412db756217202cd22
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-08 19:37:32 +0100

    new task constructor

--- src/main/java/pl/borntocode/tasker/Task.java
+++ src/main/java/pl/borntocode/tasker/Task.java
@@ -1,6 +1,8 @@
 package pl.borntocode.tasker;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
@@ -10,6 +12,7 @@ import java.sql.Timestamp;
 
 @Data
 @Entity
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Task {
 
     @ManyToOne
@@ -29,4 +32,11 @@ public class Task {
     private Date dueDate;
 
     private Timestamp timestamp;
+
+    public Task(String task, Date fromDate, Date dueDate) {
+        this.task = task;
+        this.fromDate = fromDate;
+        this.dueDate = dueDate;
+        this.timestamp = getTimestamp();
+    }
 }
