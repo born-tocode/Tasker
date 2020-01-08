85a734afdb0e6fcdfa08fc27eabbe475b389d606
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-08 19:39:10 +0100

    implemented REST Task Controller

--- src/test/java/pl/borntocode/tasker/TaskControllerTest.java
+++ src/test/java/pl/borntocode/tasker/TaskControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import pl.borntocode.tasker.web.TaskController;
+import pl.borntocode.tasker.web.api.TaskController;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
