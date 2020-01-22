a21904109b3e4382354e90a55d866343fe8e7539
Mateusz Boguszewski <mateo.b@data.pl>
2020-01-22 21:10:11 +0100

    nice error controller

--- src/main/java/pl/borntocode/tasker/web/ErrorController.java
+++ src/main/java/pl/borntocode/tasker/web/ErrorController.java
@@ -1,18 +1,22 @@
 package pl.borntocode.tasker.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Controller
-@RequestMapping("/error")
+@Slf4j
+@ControllerAdvice
 public class ErrorController {
 
-    @PostMapping
-    @GetMapping
-    public String catchError() {
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(Throwable throwable, Model model) {
+        log.error("Exception during execution of Spring application", throwable);
+        String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+        model.addAttribute("ErrorMessage", errorMessage);
         return "error";
     }
-
 }
