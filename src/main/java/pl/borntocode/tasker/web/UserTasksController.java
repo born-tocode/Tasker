32964995caa8f5a7481d6b0de8c74062bc13f7d7
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-03 17:11:47 +0100

    moved content to UserTasksController

--- src/main/java/pl/borntocode/tasker/web/UserTasksController.java
+++ src/main/java/pl/borntocode/tasker/web/UserTasksController.java
@@ -1,13 +1,19 @@
 package pl.borntocode.tasker.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import pl.borntocode.tasker.Task;
 import pl.borntocode.tasker.User;
 import pl.borntocode.tasker.data.TaskRepository;
 
+import javax.validation.Valid;
+
+@Slf4j
 @Controller
 @RequestMapping("/tasks")
 public class UserTasksController {
@@ -30,14 +36,41 @@ public class UserTasksController {
         return "alltasks";
     }
 
-    @GetMapping("/alltasks/edit/{id}")
-    public String editTask(@PathVariable @SessionAttribute Long id, Model model) {
+    @GetMapping("/alltasks/edit/**")
+    public String editTask() {
         return "edittask";
     }
 
     @PostMapping("/alltasks/edit/{id}")
-    public String saveTask(@PathVariable @SessionAttribute Long id) {
+    public String processEditTask(@PathVariable @SessionAttribute Long id) {
         taskRepository.findById(id);
+        return "redirect:/tasks/alltasks";
+    }
+
+
+    @GetMapping("/newtask")
+    public String displayNewTaskForm() {
+        return "newtask";
+    }
+
+    @PostMapping("/newtask")
+    public String processNewTask(@Valid NewTaskForm taskForm, Errors errors, @AuthenticationPrincipal User user) {
+        log.info("processNewTaskForm() started");
+
+        if (errors.hasErrors()) {
+            log.error(errors.toString());
+            return "newtask";
+        }
+
+        taskForm.setUser(user);
+        log.info("user->form ok");
+
+        Task task = new Task(taskForm);
+        log.info("form->Task ok");
+
+        taskRepository.save(task);
+        log.info("saved E Task to Repo");
+
         return "redirect:/tasks/alltasks";
     }
 
