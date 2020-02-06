c019728a0fb3b57178a9a1c094197a53455cb166
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-06 13:29:34 +0100

    SPEL works, useless HQL query

--- src/main/java/pl/borntocode/tasker/data/TaskRepository.java
+++ src/main/java/pl/borntocode/tasker/data/TaskRepository.java
@@ -1,6 +1,5 @@
 package pl.borntocode.tasker.data;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,7 +8,6 @@ import pl.borntocode.tasker.Task;
 @RepositoryRestResource
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
-    @Query("FROM Task T WHERE T.user.username=?1")
     Iterable<Task> getTasksByUserUsername(@Param("user") String user);
 
 }
