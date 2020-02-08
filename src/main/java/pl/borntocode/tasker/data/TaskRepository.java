82dec9cd638175273c20e1c4e7495a2fefa476e1
Mateusz Boguszewski <mateo.b@data.pl>
2020-02-08 20:17:52 +0100

    properly written HQL query

--- src/main/java/pl/borntocode/tasker/data/TaskRepository.java
+++ src/main/java/pl/borntocode/tasker/data/TaskRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pl.borntocode.tasker.Task;
 
+import java.sql.Date;
+
 @RepositoryRestResource
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
@@ -16,9 +18,17 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     Iterable<Task> getTasksByUserUsernameOrderByAddTimeDesc(@Param("user") String user);
     Iterable<Task> getTasksByUserUsernameOrderByFromDateAsc(@Param("user") String user);
     Iterable<Task> getTasksByUserUsernameOrderByFromDateDesc(@Param("user") String user);
-    @Query("from Task T order by case when T.priority='L' then 0 when T.priority='M' then 1 when T.priority='H' then 3 else 4 end asc")
+
+    @Query("from Task T where T.user.username= :user order by case when T.priority='L' then 0 when T.priority='M' then 1 when " +
+            "T.priority='H' then 3 else 4 end asc")
     Iterable<Task> getTasksByUserUsernameOrderByPriorityAsc(@Param("user") String user);
-    @Query("from Task T order by case when T.priority='L' then 0 when T.priority='M' then 1 when T.priority='H' then 3 else 4 end desc")
+
+    @Query("from Task T where T.user.username= :user order by case when T.priority='L' then 0 when T.priority='M' then 1 when " +
+            "T.priority='H' then 3 else 4 end desc")
     Iterable<Task> getTasksByUserUsernameOrderByPriorityDesc(@Param("user") String user);
+
+    @Query("update Task T set T.task= :task, T.priority= :priority,T.fromDate= :fromDate,T.dueDate= :dueDate where T.id= :id")
+    void getTaskByIdAndUpdate(@Param("id") Long id, @Param("task") String task, @Param("priority") String priority,
+                                @Param("fromDate")Date fromDate, @Param("dueDate") Date dueDate);
 
 }
